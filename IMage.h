f9ccd74f88a14f03f458f9cf5d64bba1045cca3f
Kirill <kipabelousov@edu.hse.ru>
2025-09-27 10:30:58 +0300

    Убрал все ненужное, оставил самую понятную логику

--- IMage.h
+++ IMage.h
@@ -9,32 +9,8 @@
 #include <QPushButton>
 #include <QFileDialog>
 #include <QMessageBox>
-#include <QTextEdit>
-#include <QScrollArea>
-
 // Функция фильтрации изображения
 void filter2D(QImage &image, double *kernel, size_t kWidth, size_t kHeight);
-
-// Класс для отображения информации об изображении
-class ImageInfoWidget : public QWidget
-{
-    Q_OBJECT
-
-private:
-    QImage *currentImage;
-    QLabel *imageLabel;
-    QTextEdit *infoText;
-    QScrollArea *scrollArea;
-
-public:
-    ImageInfoWidget(QWidget *parent = nullptr);
-    void setImage(const QImage &image);
-    void updateInfo();
-    void clear();
-
-private slots:
-    void onImageClicked();
-};
 
 // Основной виджет приложения
 class Widget : public QWidget
@@ -44,14 +20,12 @@ class Widget : public QWidget
 private:
     QImage originalImage;
     QImage filteredImage;
-    ImageInfoWidget *infoWidget;
     QLabel *imageLabel;
     QPushButton *loadButton;
     QPushButton *filterButton;
     QPushButton *resetButton;
     QVBoxLayout *mainLayout;
     QHBoxLayout *buttonLayout;
-    QHBoxLayout *imageLayout;
 
 public:
     Widget(QWidget *parent = nullptr);
--- Widget.cpp
+++ Widget.cpp
@@ -1,4 +1,4 @@
-#include "projects/IMage.h"
+#include "IMage.h"
 #include <QPainter>
 #include <QMouseEvent>
 #include <cmath>
@@ -46,6 +46,7 @@ void filter2D(QImage &image, double *kernel, size_t kWidth, size_t kHeight) {
                 a /= weightSum;
             }
 
+            // Делаем значение каждого цвета в корректных переделах пределах
             r = qBound(0.0, r, 255.0);
             g = qBound(0.0, g, 255.0);
             b = qBound(0.0, b, 255.0);
@@ -59,80 +60,12 @@ void filter2D(QImage &image, double *kernel, size_t kWidth, size_t kHeight) {
     image = result;
 }
 
-// Реализация ImageInfoWidget
-ImageInfoWidget::ImageInfoWidget(QWidget *parent) : QWidget(parent), currentImage(nullptr) {
-    setFixedSize(300, 400);
-    
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    
-    imageLabel = new QLabel("Нет изображения");
-    imageLabel->setAlignment(Qt::AlignCenter);
-    imageLabel->setStyleSheet("border: 1px solid gray; background-color: #f0f0f0;");
-    imageLabel->setFixedHeight(150);
-    
-    infoText = new QTextEdit();
-    infoText->setReadOnly(true);
-    infoText->setMaximumHeight(200);
-    
-    layout->addWidget(imageLabel);
-    layout->addWidget(new QLabel("Информация об изображении:"));
-    layout->addWidget(infoText);
-}
-
-void ImageInfoWidget::setImage(const QImage &image) {
-    currentImage = new QImage(image);
-    updateInfo();
-}
-
-void ImageInfoWidget::updateInfo() {
-    if (!currentImage || currentImage->isNull()) {
-        imageLabel->setText("Нет изображения");
-        infoText->clear();
-        return;
-    }
-
-    // Отображение миниатюры
-    QPixmap pixmap = QPixmap::fromImage(*currentImage);
-    if (pixmap.width() > 280 || pixmap.height() > 130) {
-        pixmap = pixmap.scaled(280, 130, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-    }
-    imageLabel->setPixmap(pixmap);
-
-    // Информация об изображении
-    QString info;
-    info += QString("Размер: %1 x %2 пикселей\n").arg(currentImage->width()).arg(currentImage->height());
-    info += QString("Формат: %1\n").arg(currentImage->format() == QImage::Format_RGB32 ? "RGB32" : 
-                                       currentImage->format() == QImage::Format_ARGB32 ? "ARGB32" : "Другой");
-    info += QString("Глубина цвета: %1 бит\n").arg(currentImage->bitPlaneCount());
-    info += QString("Размер в байтах: %1\n").arg(currentImage->sizeInBytes());
-    
-    // Статистика по цветам
-    int totalPixels = currentImage->width() * currentImage->height();
-    info += QString("Всего пикселей: %1\n").arg(totalPixels);
-    
-    infoText->setPlainText(info);
-}
-
-void ImageInfoWidget::clear() {
-    if (currentImage) {
-        delete currentImage;
-        currentImage = nullptr;
-    }
-    imageLabel->setText("Нет изображения");
-    infoText->clear();
-}
-
-void ImageInfoWidget::onImageClicked() {
-    // Можно добавить функционал при клике на изображение
-}
-
 // Реализация основного виджета
 Widget::Widget(QWidget *parent) : QWidget(parent) {
     setWindowTitle("Фильтрация изображений");
     resize(1200, 600);
 
     // Создание элементов интерфейса
-    infoWidget = new ImageInfoWidget(this);
     imageLabel = new QLabel("Загрузите изображение");
     imageLabel->setAlignment(Qt::AlignCenter);
     imageLabel->setStyleSheet("border: 2px solid gray; background-color: #f0f0f0;");
@@ -153,18 +86,14 @@ Widget::Widget(QWidget *parent) : QWidget(parent) {
     // Создание компоновки
     mainLayout = new QVBoxLayout(this);
     buttonLayout = new QHBoxLayout();
-    imageLayout = new QHBoxLayout();
 
     buttonLayout->addWidget(loadButton);
     buttonLayout->addWidget(filterButton);
     buttonLayout->addWidget(resetButton);
     buttonLayout->addStretch();
 
-    imageLayout->addWidget(imageLabel);
-    imageLayout->addWidget(infoWidget);
-
     mainLayout->addLayout(buttonLayout);
-    mainLayout->addLayout(imageLayout);
+    mainLayout->addWidget(imageLabel);
 }
 
 void Widget::loadImage(const QString &filename) {
@@ -172,7 +101,6 @@ void Widget::loadImage(const QString &filename) {
         filteredImage = originalImage.copy();
         imageLabel->setPixmap(QPixmap::fromImage(originalImage).scaled(
             imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-        infoWidget->setImage(originalImage);
         filterButton->setEnabled(true);
         resetButton->setEnabled(true);
     } else {
@@ -185,9 +113,9 @@ void Widget::applyFilter() {
 
     // Создание простого фильтра размытия (3x3)
     double blurKernel[9] = {
-        1.0/9, 1.0/9, 1.0/9,
-        1.0/9, 1.0/9, 1.0/9,
-        1.0/9, 1.0/9, 1.0/9
+        0.111, 0.111, 0.111,
+        0.111, 0.111, 0.111,
+        0.111, 0.111, 0.111
     };
 
     filteredImage = originalImage.copy();
@@ -195,7 +123,6 @@ void Widget::applyFilter() {
 
     imageLabel->setPixmap(QPixmap::fromImage(filteredImage).scaled(
         imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    infoWidget->setImage(filteredImage);
 }
 
 void Widget::resetImage() {
@@ -204,7 +131,6 @@ void Widget::resetImage() {
     filteredImage = originalImage.copy();
     imageLabel->setPixmap(QPixmap::fromImage(originalImage).scaled(
         imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    infoWidget->setImage(originalImage);
 }
 
 void Widget::onLoadClicked() {
